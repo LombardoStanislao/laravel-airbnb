0ec21410475cd625e29be86b45e574fa967c32ee
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2021-02-12 19:53:16 +0100

    Fix error homepage

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index');
-Route::get('/advancedsearch', 'Guest/ApartmentController');
+// Route::get('/advancedsearch', 'Guest\ApartmentController');
 
 Auth::routes();
 
