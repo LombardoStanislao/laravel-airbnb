0f9a9350e7a46223ccded540c7e76286e90b6fc3
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2021-02-12 12:12:45 +0100

    Migration Sponsorship and Sponsorship_types

--- resources/views/guest/partials/navbar-top.blade.php
+++ resources/views/guest/partials/navbar-top.blade.php
@@ -1,5 +1,5 @@
 {{-- registrazione e login --}}
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm d-none">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
