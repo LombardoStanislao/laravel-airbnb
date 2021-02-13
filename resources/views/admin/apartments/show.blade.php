222d8a4651cb3fd92faa1a0e1744ea01e3e5ca22
Valerio Farina <valerio.farina.1994@gmail.com>
2021-02-13 19:01:36 +0100

    show dettagli appartamento

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -1,11 +1,73 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
+
+@section('page-title', 'Dettagli appartamento')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <h1>Dettaglio singolo appartamento</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>{{ $apartment->title }}</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <ul>
+                    <li>
+                        <strong>Numero di stanze:</strong>
+                        <span>{{ $apartment->rooms_number }}</span>
+                    </li>
+                    <li>
+                        <strong>Numero posti letto:</strong>
+                        <span>{{ $apartment->sleeps_accomodations }}</span>
+                    </li>
+                    <li>
+                        <strong>Numero di bagni:</strong>
+                        <span>{{ $apartment->bathrooms_number }}</span>
+                    </li>
+                    <li>
+                        <strong>Metri quadrati:</strong>
+                        <span>{{ $apartment->mq }}</span>
+                    </li>
+                    <li>
+                        <strong>Prezzo per notte:</strong>
+                        <span>€ {{ $apartment->price_per_night }}</span>
+                    </li>
+                    <li>
+                        <strong>Lat:</strong>
+                        <span>{{ $apartment->latitude }},</span>
+                        <strong>Lon:</strong>
+                        <span>{{ $apartment->longitude }}</span>
+                    </li>
+                    <li>
+                        <strong>Disponibile:</strong>
+                        <span>{{ $apartment->available ? 'Sì' : 'No' }}</span>
+                    </li>
+                    <li>
+                        <strong>Comfort:</strong>
+                        @if ($apartment->comforts->isNotEmpty())
+                            @foreach ($apartment->comforts as $comfort)
+                                <span>
+                                    {{ !$loop->last ? $comfort->name . ', ' : $comfort->name }}
+                                </span>
+                            @endforeach
+                        @else
+                            <span>nessuno</span>
+                        @endif
+                    </li>
+                    <li class="w-50">
+                        <strong>Immagine di copertina:</strong>
+                        <img src="{{ asset("storage/" . $apartment->{"main-image"}) }}" class="d-block mw-100">
+                    </li>
+                    <li>
+                        <strong>Descrizione:</strong>
+                        @if ($apartment->description)
+                            <span>{{ $apartment->description }}</span>
+                        @else
+                            <span>Non disponibile</span>
+                        @endif
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
-</div>
 @endsection
