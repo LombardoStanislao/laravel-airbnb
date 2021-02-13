9a8f9802405de1294ecbb3d080b05d1d7dd46bec
Valerio Farina <valerio.farina.1994@gmail.com>
2021-02-13 11:02:19 +0100

    layout base home dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,17 +1,17 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <h1>Dashbord Admin</h1>
-            <a href="{{route('admin.apartments.index')}}" class="btn btn-info">
+    <div class="row">
+        <div class="col-12">
+            <h1>Benvenuto {{ Auth::user()->name }}</h1>
+            {{-- <a href="{{route('admin.apartments.index')}}" class="btn btn-info">
                 Pagina Index ApartmentController
             </a>
             <a href="{{route('admin.apartments.create')}}" class="btn btn-info">
                 Pagina Create ApartmentController
-            </a>
-            
+            </a> --}}
+
         </div>
     </div>
 </div>
