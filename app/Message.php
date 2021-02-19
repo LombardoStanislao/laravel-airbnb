7594378700ac7ef55e14fd69bd9f301529875ec8
Daoud <99m.daoud@gmail.com>
2021-02-19 12:24:12 +0100

    Added show received messages

--- app/Message.php
+++ app/Message.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     protected $fillable = [ "body_message", "mail_sender", "apartment_id" ];
+    
     public function apartment() {
         return $this->belongsTo('App\Apartment');
     }
